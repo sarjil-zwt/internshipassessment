ebc33d5cb0edff74698ad9a25cae41f405926ac8
sarjil-zwt <sarjil@zealousweb.com>
2024-03-06 20:13:22 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import Entries from "./pages/Entries/Entries";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/entries" element={<Entries />} />
       </Routes>
     </div>
   );
